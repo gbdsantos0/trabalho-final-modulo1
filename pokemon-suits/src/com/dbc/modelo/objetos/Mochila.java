ed7d95fcf39f13f878b82233cdcd4a8537e6ad3e
Your Name <you@example.com>
2022-02-25 17:36:37 -0300

    Adicionado a pasta objetos e adicionado dentro desta pasta a mochila e a pokedex

---
+++ pokemon-suits/src/com/dbc/modelo/objetos/Mochila.java
@@ -0,0 +1,25 @@
+package com.dbc.modelo.objetos;
+
+import com.dbc.modelo.entidades.Pokemon;
+
+import java.util.ArrayList;
+
+public class Mochila {
+    public ArrayList<Pokemon> bag = new ArrayList<Pokemon>();
+
+    public Mochila() {
+
+    }
+
+    // C Criar objeto (pokemom)
+
+
+    //  R  leitura
+
+
+    // U   atualizar objeto (pokemom)
+
+
+    // D   Remoção (remover pokemom)
+
+}
