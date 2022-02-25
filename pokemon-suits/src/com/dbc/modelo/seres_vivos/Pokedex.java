c30953ba058dbc414a196dee5253faeaa84ba2c1
flavio <flaviosguian@gmail.com>
2022-02-25 14:31:12 -0300

    feat: lista de  pokemons comentados!

--- pokemon-suits/src/com/dbc/modelo/seres_vivos/Pokedex.java
+++ pokemon-suits/src/com/dbc/modelo/seres_vivos/Pokedex.java
@@ -25,3 +25,117 @@ public class Pokedex {
 
 
 }
+
+//        #001
+//        nome:Bulbasaur
+//        idade:
+//        peso:
+//        sexo: M
+//        dificuldade:
+//        pokemomtipo:grass / poison
+//        raridade:
+//
+//
+//        ######################################################
+//        #002
+//        nome: Ivysaur
+//        idade:
+//        peso:
+//        sexo: M
+//        dificuldade:
+//        pokemomtipo:grass / poison
+//        raridade:
+//
+//
+//        #######################################################
+//        #003
+//        nome:Venusaur
+//        idade:
+//        peso:
+//        sexo: M
+//        dificuldade:
+//        pokemomtipo:grass / poison
+//        raridade:
+//
+//        #######################################################
+//        #004
+//        nome:Charmander
+//        idade:
+//        peso:
+//        sexo: M
+//        dificuldade:
+//        pokemomtipo:Fire
+//        raridade:
+//
+//
+//        ########################################################
+//        #005
+//        nome:Charmeleon
+//        idade:
+//        peso:
+//        sexo: M
+//        dificuldade:
+//        pokemomtipo:Fire
+//        raridade:
+//
+//        ########################################################
+//        #006
+//        nome:Charizard
+//        idade:
+//        peso:
+//        sexo: M
+//        dificuldade:
+//        pokemomtipo:Fire / Flying
+//        raridade:
+//
+//        ######################################################
+//        #007
+//        nome:Squirtle
+//        idade:
+//        peso:
+//        sexo: f
+//        dificuldade:
+//        pokemomtipo:Water
+//        raridade:
+//
+//        #####################################################
+//
+//        #008
+//        nome:wartortle
+//        idade:
+//        peso:
+//        sexo: f
+//        dificuldade:
+//        pokemomtipo:water
+//        raridade:
+//
+//        #####################################################
+//        #009
+//        nome:Blastoise
+//        idade:
+//        peso:
+//        sexo: f
+//        dificuldade:
+//        pokemomtipo:water
+//        raridade:
+//
+//        #####################################################
+//        #010
+//        nome:Caterpie
+//        idade:
+//        peso:
+//        sexo: f
+//        dificuldade:
+//        pokemomtipo:bug
+//        raridade:
+//
+//        #####################################################
+//
+//        #011
+//        nome:Metapod
+//        idade:
+//        peso:
+//        sexo: f
+//        dificuldade:
+//        pokemomtipo:bug
+//        raridade:
