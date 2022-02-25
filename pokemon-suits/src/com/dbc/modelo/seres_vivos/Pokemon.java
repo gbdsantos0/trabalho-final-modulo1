4d50b9d5428639e4871b6564410f8cbf2e1aa07e
Your Name <you@example.com>
2022-02-24 23:30:18 -0300

    Seres_Vivos: esqueletos das classes pokemon, servivo e treinador finalizados

--- pokemon-suits/src/com/dbc/modelo/seres_vivos/Pokemon.java
+++ pokemon-suits/src/com/dbc/modelo/seres_vivos/Pokemon.java
@@ -1,4 +1,13 @@
 package com.dbc.modelo.seres_vivos;
 
-public class Pokemon {
+import com.dbc.modelo.enums.TipoPokemon;
+
+public class Pokemon extends SerVivo {
+    //substituir objects por enum
+    private Object dificuldade;
+    private int level;
+    private TipoPokemon[] tipo = new TipoPokemon[2];
+    private Object raridade;
+
+
 }
--- pokemon-suits/src/com/dbc/modelo/seres_vivos/Treinador.java
+++ pokemon-suits/src/com/dbc/modelo/seres_vivos/Treinador.java
@@ -1,4 +1,17 @@
 package com.dbc.modelo.seres_vivos;
 
-public class Treinador {
+import com.dbc.modelo.pokebolas.Pokebola;
+
+public class Treinador extends SerVivo {
+    //substituir objects por PokeDex
+    private Object pokedex;
+
+    public boolean capturar(Pokebola pokebola, Pokemon pokemon){
+        return false;
+    }
+
+    //getter e setter
+    public Object getPokedex() {return pokedex;}
+    public void setPokedex(Object pokedex) {this.pokedex = pokedex;}
+
 }
