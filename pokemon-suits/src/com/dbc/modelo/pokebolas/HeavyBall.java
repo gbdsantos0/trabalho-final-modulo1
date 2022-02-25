f3660893ec6d2fd1e1314a90f15728c48b9c1aca
Gustavo Barbosa <gbdsantos0@inf.ufpel.edu.br>
2022-02-25 14:48:32 -0300

    feat: netball(pronta) e heavyball(quase pronta)

--- pokemon-suits/src/com/dbc/modelo/pokebolas/HeavyBall.java
+++ pokemon-suits/src/com/dbc/modelo/pokebolas/HeavyBall.java
@@ -5,6 +5,20 @@ import com.dbc.modelo.seres_vivos.Pokemon;
 public class HeavyBall extends Pokebola{
         @Override
         public Double calcularChance(Pokemon pokemon){
+            switch ((int)(pokemon.getPeso()/102.35)){
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + pokemon.getPeso());
+            }
             return 1.0;
         }
 }
--- pokemon-suits/src/com/dbc/modelo/pokebolas/NetBall.java
+++ pokemon-suits/src/com/dbc/modelo/pokebolas/NetBall.java
@@ -1,10 +1,15 @@
 package com.dbc.modelo.pokebolas;
 
+import com.dbc.modelo.enums.TipoPokemon;
 import com.dbc.modelo.seres_vivos.Pokemon;
 
 public class NetBall extends Pokebola{
     @Override
     public Double calcularChance(Pokemon pokemon){
+
+        if(pokemon.getTipo()[0]==TipoPokemon.WATER||pokemon.getTipo()[0]==TipoPokemon.BUG||pokemon.getTipo()[1]==TipoPokemon.WATER||pokemon.getTipo()[1]==TipoPokemon.BUG){
+            return 3.0;
+        }
         return 1.0;
     }
 }
