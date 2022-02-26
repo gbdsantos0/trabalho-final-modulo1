29bf0ac9e6e220f8dbc15ae6b3b797cc37b445ef
Your Name <you@example.com>
2022-02-25 21:30:40 -0300

    Organizando e comentando as classes do pacote entidades

--- pokemon-suits/src/com/dbc/modelo/entidades/Treinador.java
+++ pokemon-suits/src/com/dbc/modelo/entidades/Treinador.java
@@ -9,17 +9,21 @@ import com.dbc.modelo.objetos.Pokedex;
 import java.util.Random;
 
 public class Treinador extends Entidade implements Impressao {
-
-    private final Mochila mochila = new Mochila();
+    //Instacia da mochila que guarda todos os poquemons do treinador
+    private Mochila mochila;
+    //Instancia da PokeDex, que guarda todos os Pokemons do Jogo
     private final Pokedex pokedex;
 
     public Treinador(String nome, int idade, double peso, Utils sexo, Pokedex pokedex) {
         super(nome, idade, peso, sexo);
         this.pokedex = pokedex;
+        this.mochila = new Mochila();
     }
 
+    //Metodo que retorna um boolean se o pokemon for capturado(a chance de captura é delegada para a pokebola)
     public boolean capturar(Pokebola pokebola, Pokemon pokemon, Random r){
         if(r.nextInt(100) <= pokebola.calcularChance(pokemon)){
+            //trocar futuramente pelo metodo adiciona do CRUD
             this.mochila.bag.add(pokemon);
             return true;
         }else {
@@ -29,7 +33,7 @@ public class Treinador extends Entidade implements Impressao {
 
     @Override
     public void imprimir() {
-        mochila.bag.forEach(p -> System.out.println("======================\n" + p + "============================\n"));
+        this.mochila.bag.forEach(p -> System.out.println("======================\n" + p + "============================\n"));
     }
 
     //getter
