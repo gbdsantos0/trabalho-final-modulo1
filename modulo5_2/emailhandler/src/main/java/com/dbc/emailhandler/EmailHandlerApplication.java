14ee5134b85c87d62abe887de7aa2f9eb1b27f43
Your Name <you@example.com>
2022-04-13 18:36:28 -0300

    e

---
+++ modulo5_2/emailhandler/src/main/java/com/dbc/emailhandler/EmailHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.emailhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailHandlerApplication.class, args);
+	}
+
+}
---
+++ modulo5_2/emailhandler/src/test/java/com/dbc/emailhandler/EmailHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.emailhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
